c506c0da852b771a4a89962c26e538d430d13647
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-08-03 00:58:00 +0200

    Initial commit.

---
+++ FileSystemWatcher/FileSystemWatcher/Models/Actions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileSystemWatcher.Models
+{
+    public enum Actions
+    {
+        Created,
+        Changed,
+        Renamed,
+        Deleted
+    }
+}
---
+++ FileSystemWatcher/FileSystemWatcher/Models/FileSystemAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileSystemWatcher.Models
+{
+    public class FileSystemAction
+    {
+        public string Filename { get; set; }
+        public Actions Action { get; set; }
+        public DateTime ActionTime { get; set; }
+    }
+}
